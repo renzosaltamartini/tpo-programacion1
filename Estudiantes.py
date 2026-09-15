82e75842cc4d4383451f34b1466767d122e7a2ad
Martin <zitolamartin@gmail.com>
2026-09-15 18:52:39 -0300

    cambios minimos

--- Estudiantes.py
+++ Estudiantes.py
@@ -12,7 +12,7 @@ def registrar_estudiantes(matriz_estudiantes, matriz_sesiones, matriz_asistencia
         # Se valida que el legajo no exista ya y sea un numero valido (mayor a 0)
         alumno.append(verificacion_legajo(legajo,matriz_estudiantes))
 
-        nombre=input("ingrese el nombre del estudiante: ")
+        nombre=input("ingrese el nombre completo del estudiante: ")
         # Se valida que el nombre no este repetido
         alumno.append(validar_nombre(nombre,matriz_estudiantes))
 
@@ -54,7 +54,7 @@ def verificacion_legajo(legajo,matriz_estudiantes):
     valido=True
 
     # Si la matriz esta vacia, no hay nada contra que comparar: alcanza con que sea mayor a 0
-    if len(matriz_estudiantes)==0 and legajo>0 and legajo!=0:
+    if len(matriz_estudiantes)==0 and legajo>0:
         return legajo
     else:
 
@@ -65,7 +65,6 @@ def verificacion_legajo(legajo,matriz_estudiantes):
                 # por eso "legajo in matriz_estudiantes[i]" chequea si ese legajo ya fue usado
                 if legajo in matriz_estudiantes[i] or legajo<0 or legajo==0:
                     legajo=int(input("el legajo no es valido, ingrese uno nuevo: "))
-                    i=0  # reinicia el contador para volver a validar el nuevo legajo desde el principio
                     valido=False
 
             if  valido==True:
@@ -78,6 +77,8 @@ def validar_nombre(nombre,matriz_estudiantes):
     valido=True
 
     if len(matriz_estudiantes)==0:
+        while not nombre.replace(" ", "").isalpha():#verifica que solo haya letras y elimina el espacio para poder analizar el nombnre
+            nombre=input("el nombre no es valido, hay numeros dentro del nombre, porfavor ingrese uno nuevo: ")
         return nombre
     else:
 
@@ -88,11 +89,9 @@ def validar_nombre(nombre,matriz_estudiantes):
             for i in range(len(matriz_estudiantes)):
                 # matriz_estudiantes[i][1] es la posicion donde se guarda el nombre de cada alumno
                 if nombre == matriz_estudiantes[i][1]:
-                    i=0  # reinicia el contador para volver a validar el nuevo nombre desde el principio
                     nombre=input("el nombre no es valido, ingrese uno nuevo: ")
                     valido=False
-                elif not nombre.isalpha():#verifico que solo haya letras en el nombre
-                    i=0  # reinicia el contador para volver a validar el nuevo nombre desde el principio
+                elif not nombre.replace(" ", "").isalpha():#verifico que solo haya letras en el nombre y reemplaza los espacios por nada asi el codigo lo entiende
                     nombre=input("el nombre no es valido, hay numeros dentro del nombre, porfavor ingrese uno nuevo: ")
                     valido=False
 
@@ -133,7 +132,6 @@ def buscar_estudiantes(matriz_estudiantes):
     print()
     print("Buscar Estudiante")
     buscar=0
-    alumno=[]
     encontrado=0
 
     if len(matriz_estudiantes)==0:
@@ -150,13 +148,10 @@ def buscar_estudiantes(matriz_estudiantes):
                 encontrado=1
 
                 # Se copian los datos del alumno encontrado (legajo, nombre, estado) para imprimirlos
-                alumno.append(matriz_estudiantes[i][0])
-                alumno.append(matriz_estudiantes[i][1])
-                alumno.append(matriz_estudiantes[i][2])
 
-                print(" Legajo   Nombre   Estado   ")
-                print(" %3d"%alumno[0],"   %3s"%alumno[1],"   %3s"%alumno[2])
-                    
+                print("{:<8}   {:<15}   {:<10}".format("Legajo","Nombre","Estado"))#el porcentaje menor alinea todo a la izquierda
+                print("{:<8}   {:<15}   {:<10}".format(matriz_estudiantes[i][0],matriz_estudiantes[i][1],matriz_estudiantes[i][2]))
+            
     if encontrado==0:
         print()
         print("el legajo no existe o no es valido")
@@ -212,8 +207,8 @@ def modificar_estudiantes(matriz_estudiantes):
 
                     if opcion==1 or opcion==2:
                         print("registro del alumno modificado: ")
-                        print(" Legajo   Nombre   Estado   ")
-                        print(" %3d"%matriz_estudiantes[i][0],"   %3s"%matriz_estudiantes[i][1],"   %3s"%matriz_estudiantes[i][2])
+                        print("{:<8}   {:<15}   {:<10}".format("Legajo","Nombre","Estado"))#el porcentaje menor alinea todo a la izquierda
+                        print("{:<8}   {:<15}   {:<10}".format(matriz_estudiantes[i][0],matriz_estudiantes[i][1],matriz_estudiantes[i][2]))
 
 
                     else:
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from gordo import *
-from santichad import *
-from Pollo import *
-from renzo import *
+from Estudiantes import *
+from Asistencias import *
+from Sesiones import *
+from Consultas import *
 
 
 def main():
@@ -12,7 +12,6 @@ def main():
     salida=1
     while salida!=0:
         print("-"*10,"Gestion de asistencia","-"*10)
-        print()
         print("\n 1. Menu de estudiantes \n 2. Menu de asistencia \n 3. Menu de sesiones \n 4. Menu de consultas \n 5. Finalizar Programa")
         salida=int(input("Seleccione una opcion: "))
 
@@ -50,6 +49,3 @@ def main():
             print("Opcion invalida, intente nuevamente")
 
 main()
-
-
-        
